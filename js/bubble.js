5ce82860fb37d65d2da991d7a66c6ebb0d14d35e
zschoenb <zschoenb@ualberta.ca>
2016-12-19 16:02:49 -0700

    undo test

--- js/bubble.js
+++ js/bubble.js
@@ -11,17 +11,22 @@ var pack = d3.pack()
 
 
 
-var table = d3.csvParse("data/era.csv");
+d3.csv("data/eraFlare.csv", function(d) {
+  d.value = +d.value;
+  if (d.value) return d;
+}, function(error, classes) {
+  if (error) throw error;
 
-var root = d3.stratify()
-  .id(function(d) { return d.name; })
-  .parentId(function(d) { return d.parent; })
-  (table);
-  d.id = id;
-  d.package = id.slice(0, i);
-  d.class = id.slice(i + 1);
-  
-
+  var root = d3.hierarchy({children: classes})
+      .sum(function(d) { return d.value; })
+      .each(function(d) {
+        if (id = d.data.id) {
+          var id, i = id.lastIndexOf(".");
+          d.id = id;
+          d.package = id.slice(0, i);
+          d.class = id.slice(i + 1);
+        }
+      });
 
   var node = svg.selectAll(".node")
     .data(pack(root).leaves())
@@ -50,3 +55,4 @@ var root = d3.stratify()
 
   node.append("title")
       .text(function(d) { return d.id + "\n" + format(d.value); });
+});
