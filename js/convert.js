d788d4cc6c9e9d9ade3d8fd9dfd7242e00e8773e
zschoenb <zschoenb@ualberta.ca>
2016-12-20 16:12:35 -0700

    test

--- js/convert.js
+++ js/convert.js
@@ -4,18 +4,18 @@ d3.csv("data/era.csv", function(data) {
     return d3.nest()
       .key(function(d) { 
         // console.log('key for: ' + JSON.stringify(d));
-        if (d.filename.indexOf('/') === -1) return d.filename;
-        else return d.filename.substr(0, d.filename.indexOf('/')); 
+        if (d.type.indexOf('/') === -1) return d.type;
+        else return d.type.substr(0, d.type.indexOf('/')); 
       })
       .rollup(function(v) {
           // console.log('rollup: ' + JSON.stringify(v));
           // leaf
-          if (v.length === 1 && v[0].filename.indexOf('/') === -1) {
+          if (v.length === 1 && v[0].type.indexOf('/') === -1) {
             // return {'value': parseInt(v[0].sloc)};
             return {'size': parseInt(v[0].sloc)};
           }
           // node
-          v.map(function(d) { d.filename = d.filename.substring(d.filename.indexOf('/')+1); return d; });
+          v.map(function(d) { d.type = d.type.substring(d.type.indexOf('/')+1); return d; });
           return {'children': level(v)};
       })
       .entries(nodes)
@@ -25,6 +25,6 @@ d3.csv("data/era.csv", function(data) {
       });
   };
 
-  var root = {'name': 'firefox', 'children': level(data)};
+  var root = {'name': 'description', 'children': level(data)};
 
 });
