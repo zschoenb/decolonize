4b69636bfeb3c9d36baab978758b561392c8e287
zschoenb <zschoenb@ualberta.ca>
2016-12-20 11:23:10 -0700

    conversion kit

--- js/convert.js
+++ js/convert.js
@@ -1,6 +1,8 @@
-var table = d3.csvParse("data/eraFlare.csv");
+function init() {
+	var table = d3.csvParse("data/eraFlare.csv");
 //var root = d3.stratify()
 //    .id(function(d) { return d.name; })
 //    .parentId(function(d) { return d.parent; })
 //    (table);
-return(table);
+	return table;
+}
