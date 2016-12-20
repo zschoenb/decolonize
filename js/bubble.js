9597afcc624d02045e10096f34f2f5b0254fe6d8
zschoenb <zschoenb@ualberta.ca>
2016-12-20 11:02:27 -0700

    new bubble chart

--- js/bubble.js
+++ js/bubble.js
@@ -1,61 +1,79 @@
-var svg = d3.select("svg"),
-    width = +svg.attr("width");
-
-var format = d3.format(",d");
-
-var color = d3.scaleOrdinal(d3.schemeCategory20c);
-
-var pack = d3.pack()
-    .size([width, width])
+var diameter = 960,
+    format = d3.format(",d"),
+    color = d3.scaleOrdinal(d3.schemeCategory20c);
+var bubble = d3.pack()
+    .size([diameter, diameter])
     .padding(1.5);
+var svg = d3.select("body").append("svg")
+    .attr("width", diameter)
+    .attr("height", diameter)
+    .attr("class", "bubble");
 
-updateData("data/eraFlare.csv")
 
-function updateData(dataSource) {
+function updateEra() {
 
-d3.csv(dataSource, function(d) {
-    d.value = +d.value;
-    if (d.value) return d;
-  }, function(error, classes) {
+  d3.csvParse("data/eraFlare.csv", function(error, data) {
     if (error) throw error;
-
-    var root = d3.hierarchy({children: classes})
+    var root = d3.stratify(classes(data))
         .sum(function(d) { return d.value; })
-        .each(function(d) {
-          if (id = d.data.id) {
-            var id, i = id.lastIndexOf(".");
-            d.id = id;
-            d.package = id.slice(0, i);
-            d.class = id.slice(i + 1);
-          }
-        });
-
+        .sort(function(a, b) { return b.value - a.value; });
+    bubble(root);
     var node = svg.selectAll(".node")
-      .data(pack(root).leaves())
+        .data(root.children)
       .enter().append("g")
         .attr("class", "node")
         .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
-
-    node.append("circle")
-        .attr("id", function(d) { return d.id; })
-        .attr("r", function(d) { return d.r; })
-        .style("fill", function(d) { return color(d.package); });
-
-    node.append("clipPath")
-        .attr("id", function(d) { return "clip-" + d.id; })
-      .append("use")
-        .attr("xlink:href", function(d) { return "#" + d.id; });
-
-    node.append("text")
-        .attr("clip-path", function(d) { return "url(#clip-" + d.id + ")"; })
-      .selectAll("tspan")
-      .data(function(d) { return d.class.split(/(?=[A-Z][^A-Z])/g); })
-      .enter().append("tspan")
-        .attr("x", 0)
-        .attr("y", function(d, i, nodes) { return 13 + (i - nodes.length / 2 - 0.5) * 10; })
-        .text(function(d) { return d; });
-
     node.append("title")
-        .text(function(d) { return d.id + "\n" + format(d.value); });
+        .text(function(d) { return d.data.className + ": " + format(d.value); });
+    node.append("circle")
+        .attr("r", function(d) { return d.r; })
+        .style("fill", function(d) { 
+          return color(d.data.packageName); 
+        });
+    node.append("text")
+        .attr("dy", ".3em")
+        .style("text-anchor", "middle")
+        .text(function(d) { return d.data.className.substring(0, d.r / 3); });
+  });
+
+}
+
+function updatePeel() {
+  
+  d3.json("data/peelsteele.json", function(error, data) {
+    if (error) throw error;
+    var root = d3.hierarchy(classes(data))
+        .sum(function(d) { return d.value; })
+        .sort(function(a, b) { return b.value - a.value; });
+    bubble(root);
+    var node = svg.selectAll(".node")
+        .data(root.children)
+      .enter().append("g")
+        .attr("class", "node")
+        .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+    node.append("title")
+        .text(function(d) { return d.data.className + ": " + format(d.value); });
+    node.append("circle")
+        .attr("r", function(d) { return d.r; })
+        .style("fill", function(d) { 
+          return color(d.data.packageName); 
+        });
+    node.append("text")
+        .attr("dy", ".3em")
+        .style("text-anchor", "middle")
+        .text(function(d) { return d.data.className.substring(0, d.r / 3); });
   });
 }
+
+
+// Returns a flattened hierarchy containing all leaf nodes under the root.
+function classes(root) {
+  var classes = [];
+  function recurse(name, node) {
+    if (node.children) node.children.forEach(function(child) { recurse(node.name, child); });
+    else classes.push({packageName: name, className: node.name, value: node.size});
+  }
+  recurse(null, root);
+  return {children: classes};
+}
+d3.select(self.frameElement).style("height", diameter + "px");
