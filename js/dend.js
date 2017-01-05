9c2494ce2f99e020a204bd9e041d49b7c4ef419e
zschoenb <zschoenb@ualberta.ca>
2017-01-05 09:53:26 -0700

    edit tip

--- js/dend.js
+++ js/dend.js
@@ -58,7 +58,7 @@ function update(source) {
     }
   };
   childCount(0, root);  
-  var newHeight = d3.max(levelWidth) * 47; // 20 pixels per line  
+  var newHeight = d3.max(levelWidth) * 48; // 20 pixels per line  
   tree = tree.size([newHeight, width]).sort(function(a,b){
       return a.name.toLowerCase().localeCompare(b.name.toLowerCase());
     });
