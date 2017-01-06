5ff2a7fcbe164c64a0fe3eeaacecc7d55a8f38fd
zschoenb <zschoenb@ualberta.ca>
2017-01-06 15:09:01 -0700

    edit list

--- js/list.js
+++ js/list.js
@@ -1,9 +1,3 @@
-  var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.right + margin.left)
-    .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
   $( function() {
     availableTags = d3.csv("data/all-original.csv", function(error, data) {
       if (error) throw error;
@@ -11,6 +5,4 @@
         source: availableTags
       });
     });
-  } );
-
-  }
+  });
