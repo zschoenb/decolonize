9d87e5420173c0fe45fd49a3f02a2633183b49cf
zschoenb <zschoenb@ualberta.ca>
2017-01-06 11:25:40 -0700

    edit second dendrogram

--- js/dend2.js
+++ js/dend2.js
@@ -148,9 +148,9 @@
     .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
 
     nodeEnter.append("text")
-      .attr("x", function(d) { return d.children || d._children ? 100 : 110; })
+      .attr("x", function(d) { return d.children || d._children ? 10 : 10; })
       .attr("dy", ".35em")
-      .attr("text-anchor", function(d) { return d.children || d._children ? "end" : "start"; })
+      .attr("text-anchor", function(d) { return d.children || d._children ? "start" : "start"; })
       .text(function(d) { return d.name; })
       .style("fill-opacity", 1e-6);
 
