c8d7a9c95a217abcac10062cecf946869459ac53
zschoenb <zschoenb@ualberta.ca>
2017-01-06 15:00:46 -0700

    edit list

--- js/list.js
+++ js/list.js
@@ -4,43 +4,9 @@
       .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-
-
-
-  var options = dropDown.selectAll("option")
-                  .data(data) // eg., data = [ {'value': 10}, {'value': 11}, {'value': 12} ]
-                  .enter()
-                  .append("option");
-  
-  svg.selectAll("search").append("option")
-    .data([data])
-    .attr("class", "line")
-    .attr("d", valueline);
-
-updateSearch(data)
-
-
-   var updateWindow = function(data) {
-
-    data.forEach(function(currentValue, index) {
-      var text = d3.selectAll("g").selectAll("text")
-      .data(currentValue, function(d) { return d });
-    
-    text.attr("class", "update");
-
-    text.enter().append("text")
-        .attr("class", "enter")
-        .attr("dy", ".35em")
-        .text(function(d) { return d.name; })
-
-    
-    text.exit().remove();
-  });
-
-
   $( function() {
     availableTags = d3.csv("data/all-original.csv", function(error, data) {
-      if (error) throw error;()
+      if (error) throw error;
       data.forEach(function(d) {
         source: d.Source,
         community: d.Community,
