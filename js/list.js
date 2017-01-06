3b09893457b089f7aaf50cb2d78aba0987cc65e5
zschoenb <zschoenb@ualberta.ca>
2017-01-06 15:33:02 -0700

    edit list

--- js/list.js
+++ js/list.js
@@ -5,10 +5,9 @@ $(function() {
 
     });
     var auto = function(data) {
-      console.log(d3.data(data, function(d) { return d.term }); )
-      availableTags = d3.data(data, function(d) { return d.term }); 
+
       $( "#search" ).autocomplete({
-        source: availableTags
+        source: d3.data(data, function(d) { return d.term } )
       });
     }
   });
