96bf1c223044cd8b58b2b504391d5c3a09ea378c
zschoenb <zschoenb@ualberta.ca>
2017-01-06 15:25:30 -0700

    edit list

--- js/list.js
+++ js/list.js
@@ -1,10 +1,8 @@
 $(function() {
     availableTags = d3.csv("data/all-original.csv", function(error, data) {
       if (error) throw error;
-      console.log(d.term)
-      function(d){
-        auto(d.term);
-      }
+      console.log(data.term)
+
     });
     var auto = function(availableTags) {
       console.log(availableTags)
