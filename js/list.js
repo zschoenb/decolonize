790dc13330e959efad9475d67f3cb1087334f79d
zschoenb <zschoenb@ualberta.ca>
2017-01-06 14:59:24 -0700

    edit list

--- js/list.js
+++ js/list.js
@@ -1,57 +1,3 @@
-//basically a way to get the path to an object
-  function searchTree(obj,search,path){
-    if(obj.name === search){ //if search is found return, add the object to the path and return it
-      path.push(obj);
-      return path;
-    }
-    else if(obj.children || obj._children){ //if children are collapsed d3 object will have them instantiated as _children
-      var children = (obj.children) ? obj.children : obj._children;
-      for(var i=0;i<children.length;i++){
-        path.push(obj);// we assume this path is the right one
-        var found = searchTree(children[i],search,path);
-        if(found){// we were right, this should return the bubbled-up path from the first if statement
-          return found;
-        }
-        else{//we were wrong, remove this parent from the path and continue iterating
-          path.pop();
-        }
-      }
-    }
-    else{//not the right object, return false so it will continue to iterate in the loop
-      return false;
-    }
-  }
-
-  function extract_select2_data(node,leaves,index){
-          if (node.children){
-              for(var i = 0;i<node.children.length;i++){
-                  index = extract_select2_data(node.children[i],leaves,index)[0];
-              }
-          }
-          else {
-              leaves.push({id:++index,text:node.name});
-          }
-          return [index,leaves];
-  }
-
-  var div = d3.select("body")
-    .append("div") // declare the tooltip div
-    .attr("class", "tooltip")
-    .style("opacity", 0);
-
-  var margin = {top: 20, right: 120, bottom: 20, left: 120},
-    width = 1800 - margin.right - margin.left,
-    height = 25000 - margin.top - margin.bottom;
-
-  var i = 0,
-    duration = 750,
-    root,
-    select2_data;
-
-
-  var diagonal = d3.svg.diagonal()
-    .projection(function(d) { return [d.y, d.x]; });
-
   var svg = d3.select("body").append("svg")
     .attr("width", width + margin.right + margin.left)
     .attr("height", height + margin.top + margin.bottom)
@@ -59,26 +5,19 @@
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
-  d3.json("data/all-original-combined.json", function(error,values){
-    root = values;
-    select2_data = extract_select2_data(values,[],0)[1];//I know, not the prettiest...
-   //update(root);
-    //init search box
-    $("#search").select2({
-      data: select2_data,
-      containerCssClass: "search"
-    });
-  });
-  //attach search box listener
-  $("#search").on("select2-selecting", function(e) {
-    var paths = searchTree(root,e.object.text,[]);
-    if(typeof(paths) !== "undefined"){
-      updateWindow(paths);
-    }
-    else{
-      alert(e.object.text+" not found!");
-    }
-  })
+
+
+  var options = dropDown.selectAll("option")
+                  .data(data) // eg., data = [ {'value': 10}, {'value': 11}, {'value': 12} ]
+                  .enter()
+                  .append("option");
+  
+  svg.selectAll("search").append("option")
+    .data([data])
+    .attr("class", "line")
+    .attr("d", valueline);
+
+updateSearch(data)
 
 
    var updateWindow = function(data) {
@@ -98,5 +37,24 @@
     text.exit().remove();
   });
 
+
+  $( function() {
+    availableTags = d3.csv("data/all-original.csv", function(error, data) {
+      if (error) throw error;()
+      data.forEach(function(d) {
+        source: d.Source,
+        community: d.Community,
+        type: d.Type,
+        aplha: d.Alpha,
+        term: d.Term,
+        count: +d.Count
+      });
+      return d.Term
+    }
+
+    $( "#tags" ).autocomplete({
+      source: availableTags
+    });
+  } );
 
   }
