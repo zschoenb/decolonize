8229b4e6c2107d9973c224c5ead5922cfef3de30
zschoenb <zschoenb@ualberta.ca>
2017-01-06 12:35:45 -0700

    edit second dendrogram

--- js/dend2.js
+++ js/dend2.js
@@ -179,7 +179,7 @@
         .style("fill-opacity", 1e-6);      
   
 
-      wrap(d3.selectAll('text'),150);
+
 
     // Transition nodes to their new position.
     var nodeUpdate = node.transition()
@@ -276,25 +276,3 @@ function mouseout(d) {
   }
 }
 
-    function wrap(text, width) {
-      text.each(function() {
-        var text = d3.select(this),
-          words = text.text().split(/\s+/).reverse(),
-          word,
-          line = [],
-          lineNumber = 0,
-          lineHeight = 1.1, // ems
-          y = text.attr("y"),
-          dy = parseFloat(text.attr("dy")),
-          tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
-        while (word = words.pop()) {
-          line.push(word);
-          tspan.text(line.join(" "));
-          if (tspan.node().getComputedTextLength() > width) {
-            line.pop();
-            tspan.text(line.join(" "));
-            line = [word];
-            tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
-          }
-        }
-        d3.select(this.parentNode.children[0]).attr('height', 19 * (lineNumber+1));
