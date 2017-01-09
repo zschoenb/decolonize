810530e8f5f5ffc0de6bd8008b90b9568969a2fa
zschoenb <zschoenb@ualberta.ca>
2017-01-09 08:38:03 -0700

    edit list

--- js/list.js
+++ js/list.js
@@ -1,13 +1,19 @@
-$(function() {
-    availableTags = d3.csv("data/all-original.csv", function(error, data) {
-      if (error) throw error;
-      auto(data);
 
-    });
-    var auto = function(data) {
-      console.log(function(d) { return d.term; } )
+    d3.csv("data/all-original.csv", function(d) {
+  return {
+    source: d.Source,
+    community: d.Community,
+    alpha: d.Aplha,
+    term: d.Term,
+    count: +d.Count // convert "Length" column to number
+  };
+}, function(error, rows) {
+  console.log(rows);
+});
+
+  $(function() {
       $( "#search" ).autocomplete({
-         source: function(d) { return d.term; } 
+         source: function(d) { return d.Term; } 
       });
     }
   });
