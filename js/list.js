12b869663f2b073ed44c2b5b2cf6d6a33beadc6a
zschoenb <zschoenb@ualberta.ca>
2017-01-09 08:47:20 -0700

    edit list

--- js/list.js
+++ js/list.js
@@ -1,12 +1,12 @@
-
 d3.csv("data/all-original.csv", function(d) {
   return {
-    source: d.Source,
-    type: d.Type,
-    community: d.Community,
-    alpha: d.Aplha,
-    term: d.Term,
-    count: +d.Count
+    d
+    //source: d.Source,
+    //type: d.Type,
+    //community: d.Community,
+    //alpha: d.Aplha,
+    //term: d.Term,
+    //count: +d.Count
   };
 }, function(error, rows) {
   console.log(rows);
