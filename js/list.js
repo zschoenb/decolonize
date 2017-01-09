8ea9c89577a527cfa36296eda7f0e834dec56bf8
zschoenb <zschoenb@ualberta.ca>
2017-01-09 09:09:46 -0700

    edit list

--- js/list.js
+++ js/list.js
@@ -11,10 +11,10 @@ d3.csv("data/all-original.csv", function(d) {
   console.log(rows);
 });
 
-console.log(function(d) { return d.term })
+console.log(function(d) { return d })
 
 $(function() {
       $( "#search" ).autocomplete({
-         source: function(d) { return d.term }
+         source: function(d) { return d }
       });
   });
