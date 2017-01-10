d7b6dcb6d0f0ef0f96516f055915b6d80e2e262d
zschoenb <zschoenb@ualberta.ca>
2017-01-10 15:58:05 -0700

    edit list

--- js/list.js
+++ js/list.js
@@ -15,7 +15,6 @@ d3.csv("data/all-original.csv", function(error, data) {
     var selectValue = d3.select('select').property('value')
     d3.select('body')
       .data(data).enter()
-      .filter(function(d) { return d.term == selectValue })
         .append('p')
         .text(function(d) { return d.source})      
        
