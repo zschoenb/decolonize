ff13c99f5ac409638a72427c27f9dee746e45ed0
zschoenb <zschoenb@ualberta.ca>
2017-01-11 08:54:31 -0700

    edit list

--- js/list.js
+++ js/list.js
@@ -3,24 +3,24 @@ d3.csv("data/all-original.csv", function(error, data) {
   var select = d3.select('body')
     .append('select')
       .attr('class','select')
-      .on('change',onchange)
+      .on('change',function(d,i) {
+      //var selectValue = d3.select('select').property('value')
+        d3.select('body')
+            .data(data)
+            .enter()
+            .append('p')
+            .text(d.value)      
+        };
 
   var options = select
     .selectAll('option')
     .data(data).enter()
     .append('option')
       .text(function (d) { return d.term; });
+
+
+    data.forEach(function(d) {
+        d.count = +d.count;
+    });
+
 });
-
-console.log(function(d){return d})
-
-function onchange() {
-    var selectValue = d3.select('select').property('value')
-    d3.select('body')
-        .data(data)
-        .enter()
-        .append('p')
-        .filter(function(d) { return d.term == selectValue })
-        .text(function(d) { return d.source})      
-       
-  };
