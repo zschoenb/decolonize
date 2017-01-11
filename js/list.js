afe35a78359f33828914dc6549ecd2466cd9b43c
zschoenb <zschoenb@ualberta.ca>
2017-01-11 10:24:32 -0700

    edit list

--- js/list.js
+++ js/list.js
@@ -13,13 +13,13 @@ d3.csv("data/all-original.csv", function(error, data) {
 
   function onchange(i) {
       var selectValue = d3.select('select').property('value')
-      var total = 0
+      d3.selectAll('p').remove()
       d3.select('body')
           .data(data)
           .enter()
           .append('p')
           .filter(function(d) { return d.term == selectValue })    
-          .text(function(d) { return d.count + ' used in ' + d.source + ': ' + d.community})
+          .text(function(d) { return d.count + ' in ' + d.source + ': ' + d.community})
 
 
     };
