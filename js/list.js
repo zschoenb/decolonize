6abe7e031448515ac0df4cb05425b0b5f392f674
zschoenb <zschoenb@ualberta.ca>
2017-01-11 09:16:24 -0700

    edit list

--- js/list.js
+++ js/list.js
@@ -18,9 +18,9 @@ d3.csv("data/all-original.csv", function(error, data) {
           .enter()
           .append('p')
           .filter(function(d) { return d.term == selectValue })
-          .text('source: ' + function(d) { return d.source})
+          .text(function(d) { return d.source})
           .append('p')
-          .text('community: ' + function(d) { return d.community})
+          .text(function(d) { return d.community})
 
          
     };
